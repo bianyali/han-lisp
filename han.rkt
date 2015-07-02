35ec210f07cdb74701bfdc1131a866c3948bc896
bianyali <bianyali.zju@gmail.com>
2015-07-02 22:34:01 +0800

    add han.rkt

---
+++ han.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (设 x y)
+  (set! x y))
+
+(define (若 x y z)
+  (if x
+      y
+      z))
+
+(define (令 x y)
+  (set! x y))
+
+(define 链 list)
+
+
+; 数字计算
+(define (加 x y)
+  (+ x y))
+
+
+; boolean
+(define 真 #t)
+
+(define 假 #f)
